9e7fa34a12dbc3c4097c58b4abb932d42c82d9af
Aditya R B <bhusanuraditya@gmail.com>
2025-04-07 14:48:35 +0530

    Added Training, Utils, Dataset and, Images

---
+++ train.py
@@ -0,0 +1,23 @@
+import torch
+from dataset import HorseZebraDataset
+import sys
+from utils import save_checkpoint, load_checkpoint
+from torchvision import transforms
+from torch.utils.data import DataLoader
+#from trochvision.datasets import ImageFolder
+imort torch.nn as nn
+import troch.optim as optim
+import config
+from tqdm import tqdm
+from torchvision.utils import save_image
+from discriminator_model import Discriminator
+from generator_model import Generator
+
+def train_fn():
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
