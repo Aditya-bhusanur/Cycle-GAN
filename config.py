bdaacfd0f830465400e3d58068cf334b8454cf24
Aditya R B <bhusanuraditya@gmail.com>
2025-04-14 14:18:55 +0530

    bugg free

--- config.py
+++ config.py
@@ -13,14 +13,15 @@ NUM_WORKERS = 4
 NUM_EPOCHS = 200
 LOAD_MODEL = False
 SAVE_MODEL = True
-CHECKPOINT_GEN_H = "gen.path.tar"
+CHECKPOINT_GEN_H = "genh.path.tar"
+CHECKPOINT_GEN_Z = "genz.path.tar"
 CHECKPOINT_CRITIC_H = "critich.pth.tar"
 CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
 
 transforms = A.Compose(
 	[
 		A.Resize(width=256, height=256),
-		A.HrizontalFlip(p=0.5),
+		A.HorizontalFlip(p=0.5),
 		A.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5], max_pixel_value=255),
 		ToTensorV2(),
 	],
